b68c70df1cc90913f77e9987ba0553f29721dd9b
simoneburrai <burrai.s98@gmail.com>
2025-12-31 12:17:35 +0000

    feat: Exercise 1 Completed

---
+++ ex1/ex1.cpp
@@ -0,0 +1,10 @@
+/*Definire una costante di tipo
+ intero e stamparne il valore.*/
+
+#include <iostream>
+
+int main(){
+	const int MY_NUMBER = 17;
+	std::cout << "My costant number is " << MY_NUMBER << std::endl;
+
+}
