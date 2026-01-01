50fa0af5cf157389a5e6f2dd0ca649aa37bf4c6d
simoneburrai <burrai.s98@gmail.com>
2026-01-01 16:58:14 +0000

    feat: ex2 completed

---
+++ ex2/ex2.cpp
@@ -0,0 +1,15 @@
+/*
+Definire una costante di tipo float
+ e stamparne il valore.
+*/
+
+#include <iostream>
+
+int main (){
+
+	const float MY_NUMBER = 12.475;
+
+	std::cout << "My costant number is " << MY_NUMBER;
+
+
+}
