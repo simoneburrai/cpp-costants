d439af5283a9b31b014f1a1faa03d5381d9573bf
simoneburrai <burrai.s98@gmail.com>
2026-01-03 15:00:56 +0100

    feat: ex5 completed

---
+++ ex5/ex5.cpp
@@ -0,0 +1,12 @@
+// Definire una costante di tipo char e stamparne il valore.
+
+#include <iostream>
+
+int main(){
+
+    const char MY_CHAR = 'S';
+
+    std::cout << "My char is " << MY_CHAR;
+
+    return 0;
+}
