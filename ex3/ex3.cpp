e1fbf051b895e07ba4291981d21005a13a47e5bb
simoneburrai <burrai.s98@gmail.com>
2026-01-03 14:48:20 +0100

    feat: ex3 completed

---
+++ ex3/ex3.cpp
@@ -0,0 +1,11 @@
+// Definire una costante stringa e stamparne il valore.
+
+#include <iostream>
+
+using std::string;
+
+int main() {
+    const string MY_STRING = "Hello Everyone";
+
+    std::cout << MY_STRING;
+}
