1c4715d99acf7852fb8a71e7851b49c6d072df67
simoneburrai <burrai.s98@gmail.com>
2026-01-03 15:56:51 +0100

    feat: ex8 completed (unsigned variables)

---
+++ ex8/ex8.cpp
@@ -0,0 +1,14 @@
+// Definire una costante di tipo unsigned int e stamparne il valore.
+
+
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    const unsigned int WEEK_DAYS = 7;
+
+    cout << "The days in a week are " << WEEK_DAYS;
+    
+    return 0;
+}
